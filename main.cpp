9a4343343eb8a9efd3ecb0e5eaf8c2cc05512984
Sebastian Klang <sebklang@gmail.com>
2022-08-07 23:18:51 +0200

    removing crystal pin 22 fixed it

--- main.cpp
+++ main.cpp
@@ -7,12 +7,11 @@
 
 int main()
 {
-    std::array<base_pin*, 5> pins = {
+    std::array<base_pin*, 4> pins = {
         new candle_pin(0),
         new candle_pin(6),
         new candle_pin(10),
 
-        new crystal_pin(22),
         new crystal_pin(28)
     };
 
